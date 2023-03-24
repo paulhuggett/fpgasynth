paulhuggett/fpgasynth

commit 9dbd85cbb48d44ffe6dd0e34a839ff0c8abccd0b

The EG now produces 'amplitude' values.
Next, I should be able to multiply these by the output of the NCO
to produce actual tones!

lerp.sv
@@ -47,10 +47,12 @@ module lerp #(
   typedef logic signed [XR:0] qxr;
 
   always_comb begin
+/* verilator lint_off UNUSEDSIGNAL */
     automatic sinttype diff = sinttype'({1'b0, b}) - sinttype'({1'b0, a});
-    automatic qxr d = qxr'(diff) * ratio; // Qx.0*Q0.r=Qx.r
-    automatic sinttype e = d[XR:RATIO_FRAC_BITS]; // Extract the integer part.
+    automatic qxr d = (qxr'(diff) * ratio); // Qx.0*Q0.r=Qx.r
+    automatic sinttype e = sinttype'(d); // Extract the integer part.
     out = uinttype'(e + a); // Qx.0+Qx.0=Q(x+1).0 => QUx.0 (truncate to QUx.0)
+/* verilator lint_on UNUSEDSIGNAL */
   end
   
 endmodule:lerp

adsr.sv
@@ -12,6 +12,10 @@
 // "Envelope Generators and DCA" section 7.5 "Analog EG Emulation".
 
 `timescale 1 ps / 1 ps
+
+import mypackage::amplitude;
+import mypackage::AMPLITUDE_BITS;
+
 module adsr #(
   parameter TOTAL_BITS = 32,
   parameter FRACTIONAL_BITS = 16,
@@ -26,7 +30,7 @@ module adsr #(
   input logic signed [TOTAL_BITS-1:0] s,
   input logic signed [TOTAL_BITS-1:0] r,
   input logic gate,
-  output logic signed [TOTAL_BITS-1:0] out,
+  output amplitude out,
   output logic active
 );
 
@@ -36,7 +40,9 @@ module adsr #(
   `define TO_FIXED(x) (fixed'((x) * FRACTIONAL_MUL))
   `define MUL2FIX(x) (fixed'((x) >> FRACTIONAL_BITS))
 
-  localparam fixed one = fixed'(1) << FRACTIONAL_BITS;
+  // 'one' is actually the value that is just less than one!
+  localparam fixed one = ((fixed'(1) << FRACTIONAL_BITS) - fixed'(1));
+
   localparam real FRACTIONAL_MUL = 2.0 ** FRACTIONAL_BITS;
   localparam fixed ATTACK_RATIO_F  = `TO_FIXED(ATTACK_RATIO);
   localparam fixed DECAY_RATIO_F   = `TO_FIXED(DECAY_RATIO);
@@ -57,12 +63,12 @@ module adsr #(
   time_values bases_;
   time_values coefs_;
 
-  enum logic [4:0] { // Ensure that states are onehot.
-    IDLE    = 5'b00001,
-    ATTACK  = 5'b00010,
-    DECAY   = 5'b00100,
-    SUSTAIN = 5'b01000,
-    RELEASE = 5'b10000
+  enum logic [3:0] { // Ensure that states are onehot.
+    IDLE    = 4'b0000,
+    ATTACK  = 4'b0001,
+    DECAY   = 4'b0010,
+    SUSTAIN = 4'b0100,
+    RELEASE = 4'b1000
   } state_;
   fixed output_;
   logic gate_;
@@ -70,6 +76,11 @@ module adsr #(
   function mul_type sign_extend (fixed x);
     return { {TOTAL_BITS{x[TOTAL_BITS-1]}}, x[TOTAL_BITS-1:0] };
   endfunction:sign_extend
+  /* verilator lint_off UNUSEDSIGNAL */
+  function amplitude fixed2amplitude (fixed x);
+    return x[FRACTIONAL_BITS-1:FRACTIONAL_BITS-AMPLITUDE_BITS];
+  endfunction
+  /* verilator lint_on UNUSEDSIGNAL */
 
   eexp #(.TOTAL_BITS(TOTAL_BITS), .FRACTIONAL_BITS(FRACTIONAL_BITS))
     attack_exp (
@@ -113,16 +124,16 @@ module adsr #(
         end
       end
 
-      case (state_)
+      unique case (state_)
       ATTACK: begin
         automatic fixed aout = bases_.a + fixed'((sign_extend(output_) * sign_extend(coefs_.a)) >> FRACTIONAL_BITS);
         if (aout >= one) begin
           output_ <= one;
-          out <= one;
+          out <= fixed2amplitude(one);
           state_ <= DECAY;
         end else begin
           output_ <= aout;
-          out <= aout;
+          out <= fixed2amplitude(aout);
         end
       end
 
@@ -130,13 +141,14 @@ module adsr #(
         automatic fixed dout = bases_.d + fixed'((sign_extend(output_) * sign_extend(coefs_.d)) >> FRACTIONAL_BITS);
         if (dout <= s) begin
           output_ <= s;
-          out <= s;
+          out <= fixed2amplitude(s);
           state_ <= SUSTAIN;
         end else begin
           output_ <= dout;
-          out <= dout;
+          out <= fixed2amplitude(dout);
         end
       end
+
 
       RELEASE: begin
         automatic fixed rout = bases_.r + fixed'((mul_type'(output_) * coefs_.r) >> FRACTIONAL_BITS);
@@ -146,12 +158,13 @@ module adsr #(
           state_ <= IDLE;
         end else begin
           output_ <= rout;
-          out <= rout;
+          out <= fixed2amplitude(rout);
         end
       end
 
       IDLE: begin end
       SUSTAIN: begin end
+
       endcase
     end
   end

adsr_tb.sv
@@ -1,5 +1,7 @@
 `timescale 1 ps / 1 ps
 
+import mypackage::amplitude;
+
 module adsr_tb ();
 
   localparam SAMPLE_RATE = 1000;
@@ -16,7 +18,7 @@ module adsr_tb ();
   fixed s;
   fixed r;
   bit gate;
-  fixed out;
+  amplitude out;
   bit active;
 
   initial forever #1 clk = ~clk;
@@ -38,7 +40,7 @@ module adsr_tb ();
   );
 
   initial begin
-    $monitor ("[%0t] active=%d out=%f", $time, active, real'(out) / MAX);
+    $monitor ("[%0t] active=%d out=%x", $time, active, out);
 
     #1
     reset = 1'b0;

eexp.sv
@@ -17,8 +17,8 @@ module eexp #(
   typedef logic signed [TOTAL_BITS - 1:0] value_type;
   typedef logic signed [TOTAL_BITS * 2 - 1:0] mul_type;
 
-  function mul_type sign_extend (value_type x);
-    return { {TOTAL_BITS{x[TOTAL_BITS-1]}}, x[TOTAL_BITS-1:0] };
+  function mul_type sign_extend (value_type y);
+    return { {TOTAL_BITS{y[TOTAL_BITS-1]}}, y[TOTAL_BITS-1:0] };
   endfunction:sign_extend
 
   localparam value_type one = value_type'(1 * (2 ** FRACTIONAL_BITS));

mypackage.sv
@@ -1,5 +1,7 @@
 `timescale 1 ps / 1 ps
 package mypackage;
+
+/* verilator lint_off UNUSEDPARAM */
   parameter AMPLITUDE_BITS = 24;
   typedef logic[AMPLITUDE_BITS - 1:0] amplitude;
 
@@ -25,5 +27,6 @@ package mypackage;
 
   // QUa.b where a+b=PHASE_INDEX_BITS and b=PHASE_ACCUMULATOR_FRACTIONAL_BITS.
   typedef logic[PHASE_INDEX_BITS-1:0] phase_index_type;
+/* verilator lint_on UNUSEDPARAM */
 
 endpackage:mypackage

top.sv
@@ -82,7 +82,7 @@ module top(
   fixed r = fixed'((1.0 / (release_time * sample_rate)) * FRACTIONAL_MUL);//48'd44739;//fixed'((1.0 / (release_time * sample_rate)) * (2.0 ** FRACTIONAL_BITS));
   logic active;
   logic gate;
-  fixed egout;
+  amplitude egout;
 
   adsr #(
     .TOTAL_BITS(TOTAL_BITS),
@@ -101,27 +101,27 @@ module top(
   assign gate = key[0];
 
   always @(posedge audio_clock) begin
-    LED[0] <= (egout >= 48'd1 << 32);//fixed'(1 * (2 ** FRACTIONAL_BITS))) ? 1'b1 : 1'b0;
-    LED[1] <= (egout >= 48'hE000_0000);//fixed'(7.0 / 8.0 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
-    LED[2] <= (egout >= 48'hC000_0000);//fixed'(0.75 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
-    LED[3] <= (egout >= 48'hA000_0000);//fixed'(0.625 / 8.0 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
-    LED[4] <= (egout >= 48'h8000_0000);//fixed'(0.5 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
-    LED[5] <= (egout >= 48'h6000_0000);//fixed'(0.375 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
-    LED[6] <= (egout >= 48'h4000_0000);//fixed'(0.25 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
-    LED[7] <= (egout >= 48'h2000_0000);//fixed'(0.125 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
+    LED[0] <= (egout > 24'hE0_0000);//fixed'(7.0/8.0*(2**AMPLITUDE_BITS);
+    LED[1] <= (egout > 24'hC0_0000);//fixed'(7.0 / 8.0 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
+    LED[2] <= (egout > 24'hA0_0000);//fixed'(0.75 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
+    LED[3] <= (egout > 24'h80_0000);//fixed'(0.625 / 8.0 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
+    LED[4] <= (egout > 24'h60_0000);//fixed'(0.5 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
+    LED[5] <= (egout > 24'h40_0000);//fixed'(0.375 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
+    LED[6] <= (egout > 24'h20_0000);//fixed'(0.25 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
+    LED[7] <= (egout > 24'h00_0000);//fixed'(0.125 * FRACTIONAL_MUL)) ? 1'b1 : 1'b0;
   end
   always @(posedge audio_clock) begin
-    GPIO[ 5] <= egout[FRACTIONAL_BITS];//osc_out[0];
-    GPIO[ 6] <= egout[FRACTIONAL_BITS-1];//osc_out[1];
-    GPIO[ 7] <= egout[FRACTIONAL_BITS-2];//osc_out[2];
-    GPIO[ 8] <= egout[FRACTIONAL_BITS-3];//osc_out[3];
-    GPIO[ 9] <= egout[FRACTIONAL_BITS-4];//osc_out[4];
-    GPIO[10] <= egout[FRACTIONAL_BITS-5];//osc_out[5];
-    GPIO[11] <= egout[FRACTIONAL_BITS-6];//osc_out[6];
-    GPIO[12] <= egout[FRACTIONAL_BITS-7];//osc_out[7];
-    GPIO[13] <= egout[FRACTIONAL_BITS-8];//osc_out[8];
-    GPIO[14] <= egout[FRACTIONAL_BITS-9];//osc_out[9];
-    GPIO[15] <= egout[FRACTIONAL_BITS-10];//osc_out[10];
-    GPIO[16] <= egout[FRACTIONAL_BITS-11];//osc_out[11];
+    GPIO[ 5] <= egout[AMPLITUDE_BITS-1];//osc_out[0];
+    GPIO[ 6] <= egout[AMPLITUDE_BITS-2];//osc_out[1];
+    GPIO[ 7] <= egout[AMPLITUDE_BITS-3];//osc_out[2];
+    GPIO[ 8] <= egout[AMPLITUDE_BITS-4];//osc_out[3];
+    GPIO[ 9] <= egout[AMPLITUDE_BITS-5];//osc_out[4];
+    GPIO[10] <= egout[AMPLITUDE_BITS-6];//osc_out[5];
+    GPIO[11] <= egout[AMPLITUDE_BITS-7];//osc_out[6];
+    GPIO[12] <= egout[AMPLITUDE_BITS-8];//osc_out[7];
+    GPIO[13] <= egout[AMPLITUDE_BITS-9];//osc_out[8];
+    GPIO[14] <= egout[AMPLITUDE_BITS-10];//osc_out[9];
+    GPIO[15] <= egout[AMPLITUDE_BITS-11];//osc_out[10];
+    GPIO[16] <= egout[AMPLITUDE_BITS-12];//osc_out[11];
   end
 endmodule:top

verilator/CMakeLists.txt
@@ -3,12 +3,20 @@ project (cmake_example)
 find_package (verilator HINTS $ENV{VERILATOR_ROOT})
 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-warning-option")
 
-set (verilator_args --timing --assert) # -Wall
+set (verilator_args --timing --assert -Wall)
 set (vsrcs "${CMAKE_CURRENT_SOURCE_DIR}/..")
 
 add_executable (Vadsr Vadsr_tb_main.cpp Vmain.hpp sc_time_stamp.cpp)
 set_target_properties (Vadsr PROPERTIES CXX_STANDARD 17 CXX_STANDARD_REQUIRED Yes)
-verilate (Vadsr SOURCES "${vsrcs}/adsr_tb.sv" "${vsrcs}/adsr.sv" "${vsrcs}/eexp.sv" VERILATOR_ARGS ${verilator_args})
+verilate (Vadsr
+  TOP_MODULE adsr_tb
+  SOURCES
+    "${vsrcs}/mypackage.sv"
+    "${vsrcs}/adsr_tb.sv"
+    "${vsrcs}/adsr.sv"
+    "${vsrcs}/eexp.sv"
+  VERILATOR_ARGS ${verilator_args}
+)
 
 add_executable (Veexp Veexp_tb_main.cpp Vmain.hpp sc_time_stamp.cpp)
 set_target_properties (Veexp PROPERTIES CXX_STANDARD 17 CXX_STANDARD_REQUIRED Yes)

verilator/Vadsr_tb_main.cpp
@@ -1,4 +1,4 @@
 // Verilated -*- C++ -*-
 #include "Vmain.hpp"
-#include "Vadsr_tb.h"
-int main(int argc, char** argv) { return Vmain<Vadsr_tb> (argc, argv); }
+#include "Vmypackage.h"
+int main(int argc, char** argv) { return Vmain<Vmypackage> (argc, argv); }

commit 091fc79ab37434b1607a9f0762bfcdfc9883d71b

Commit 9dbd85c broke the lerp implementation. Restore the code.

lerp.sv
@@ -50,7 +50,7 @@ module lerp #(
 /* verilator lint_off UNUSEDSIGNAL */
     automatic sinttype diff = sinttype'({1'b0, b}) - sinttype'({1'b0, a});
     automatic qxr d = (qxr'(diff) * ratio); // Qx.0*Q0.r=Qx.r
-    automatic sinttype e = sinttype'(d); // Extract the integer part.
+    automatic sinttype e = sinttype'(d[XR:RATIO_FRAC_BITS]); // Extract the integer part.
     out = uinttype'(e + a); // Qx.0+Qx.0=Q(x+1).0 => QUx.0 (truncate to QUx.0)
 /* verilator lint_on UNUSEDSIGNAL */
   end